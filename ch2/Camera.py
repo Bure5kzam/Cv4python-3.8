f85f3a554626891b5bb3c1cc9dfa8b941fb430fc
Bure <bure5kzam@gmail.com>
2020-12-27 08:12:54 +0900

    [ch2-Camera]Added

---
+++ ch2/Camera.py
@@ -0,0 +1,25 @@
+import cv2
+'''
+기기에 연결된 카메라로 촬영되는 영상을 edge detection 된 영상과 일반영상으로 띄워 보여주는 코드
+'''
+cap = cv2.VideoCapture()
+cap.open(0)
+
+try:
+    if not cap.isOpened():
+        raise Exception("Camera isn't opend")
+        sys.exit()
+except:
+    print("Wrong")
+
+
+while(True):
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    edge = cv2.Canny(frame,50,150)
+    cv2.imshow("frame", frame)
+    cv2.imshow("edge", edge)
+    if cv2.waitKey(20) == 27:
+        break;
